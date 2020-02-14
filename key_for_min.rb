6b05aa91d7f8bada84414fc6e44f5ecdc0578d72
Dakota States <dakota.states@gmail.com>
2020-02-14 00:14:31 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,14 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  lkey = nil
+  lvalue = nil
+  
+  name_hash.each do |k, v|
+    if lvalue == nil || v < lvalue
+    lvalue = v
+    lkey = k
+    end
+  end
+lkey
 end
