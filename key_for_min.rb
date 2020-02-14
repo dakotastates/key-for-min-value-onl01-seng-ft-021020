86b2f9c426bb9493817782b9a16869022b79133c
Dakota States <dakota.states@gmail.com>
2020-02-14 00:12:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  lkey=nil
-  lvalue=nil
+  lkey = nil
+  lvalue = nil
   
   name_hash.each do |k, v|
     if lvalue == nil || v < lvalue
